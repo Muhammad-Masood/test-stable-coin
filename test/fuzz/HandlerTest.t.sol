4629184c0604eaaf87498c6a36c84612493424dc
Muhammad-Masood <yoursourcecoder@gmail.com>
2023-09-22 13:14:50 -0700

    test

--- test/fuzz/HandlerTest.t.sol
+++ test/fuzz/HandlerTest.t.sol
@@ -1,1 +1,40 @@
-// Arrange the functions to avoid wasting runs
+//SPDX-License-Identiifer: MIT
+pragma solidity ^0.8.21;
+
+// // Arrange the functions to avoid wasting runs
+
+// // 1. The total supply of TSC should be less than the total value of collateral
+// // 2. Getter view functions should never revert <- evergreen
+
+import {TestStableCoin} from "../../src/TestStableCoin.sol";
+import {TSCEngine} from "../../src/TSCEngine.sol";
+import {ERC20Mock} from "openzeppelin/mocks/ERC20Mock.sol";
+
+contract HandlerTest {
+    TestStableCoin tsc;
+    TSCEngine tscEngine;
+    ERC20Mock wETH;
+    ERC20Mock wBTC;
+
+
+    constructor(TestStableCoin _tsc, TSCEngine _tscEngine) {
+        tsc = _tsc;
+        tscEngine = _tscEngine;
+        wETH = ERC20Mock(tscEngine.getCollateralToken(0));
+        wBTC = ERC20Mock(tscEngine.getCollateralToken(1));
+    }
+
+    function depositCollateral(uint256 _collateralSeed, uint256 _amount) external {
+        address collateral = address(_getCollateralToken(_collateralSeed));
+        tscEngine.depositCollateral(collateral,_amount);
+    }
+
+    function _getCollateralToken(uint256 _seed) internal returns (ERC20Mock) {
+        // even case: wETH
+        if(_seed % 2 == 0){ 
+            return wETH;
+        } else {
+            return wBTC;
+        }
+    }
+}
---
+++ test/fuzz/Invariants.t.sol
@@ -0,0 +1,45 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.21;
+
+import {TestStableCoin} from "../../src/TestStableCoin.sol";
+import {TSCEngine} from "../../src/TSCEngine.sol";
+import {TSCScript} from "../../script/DeployTSC.s.sol";
+import {console} from "lib/forge-std/src/console.sol";
+import {Test} from "lib/forge-std/src/Test.sol";
+import {HelperConfig} from "../../script/HelperConfig.s.sol";
+import {ERC20Mock} from "openzeppelin/mocks/ERC20Mock.sol";
+import {ERC20} from "openzeppelin/token/ERC20/ERC20.sol";
+import {IERC20} from "openzeppelin/interfaces/IERC20.sol";
+import {MockV3Aggregator} from "../mocks/MockV3Aggregator.sol";
+import {StdInvariant} from "lib/forge-std/src/StdInvariant.sol";
+import {HandlerTest} from "./HandlerTest.t.sol";
+
+contract OpenInvariantsTest is StdInvariant, Test {
+    TestStableCoin private tsc;
+    TSCEngine private tscEngine;
+    HelperConfig private config;
+    ERC20Mock private erc20Mock;
+    address wETH;
+    address wBTC;
+
+    function setUp() external {
+        TSCScript tscScript = new TSCScript();
+        (tsc, tscEngine, config) = tscScript.run();
+        (wETH, wBTC,,,) = config.getActiveNetworkConfig();
+        // targetContract(address(tscEngine));
+        HandlerTest handler = new HandlerTest(tsc,tscEngine);
+        targetContract(address(handler));
+        // console.log("total supply: ", tsc.totalSupply());
+    }
+
+    // collateral must be greater than the total TSC minted
+    function invariant_protocolMustHaveMoreValueThanTotalSupply() public view {
+        uint256 totalSupply = tsc.totalSupply();
+        uint256 wETHDeposited = IERC20(wETH).balanceOf(address(tscEngine));
+        uint256 wBTCDeposited = IERC20(wBTC).balanceOf(address(tscEngine));
+        uint256 wETHDepositedValue = tscEngine.getUSDValue(wETH, wETHDeposited);
+        uint256 wBTCDepositedValue = tscEngine.getUSDValue(wBTC, wBTCDeposited);
+        uint256 totalCollateralValue = wETHDepositedValue + wBTCDepositedValue;
+        assert(totalCollateralValue >= totalSupply);
+    }
+}
